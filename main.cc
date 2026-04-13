e796f5c30bb692dc3f02e8828bad7e2842746bf0
binentity <binentity@yandex.ru>
2026-04-13 12:42:14 +0300

    Entry point.

--- main.cc
+++ main.cc
@@ -1,32 +1,7 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
-
-constexpr unsigned int WIDTH = 2550;
-constexpr unsigned int HEIGHT = 1440;
-
-constexpr sf::Vector2u screenVector(WIDTH, HEIGHT);
-
-constexpr std::string WINDOW_TITLE = "Happiness";
+#include "world.h"
 
 int main()
 {
-
-    std::cout << "hello world" << std::endl;
-
-    const sf::VideoMode mode(screenVector);
-    sf::RenderWindow window(mode, WINDOW_TITLE);
-
-    while (window.isOpen()) {
-
-        while (const std::optional<sf::Event> event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-            }
-        }
-
-        window.clear(sf::Color::Green);
-
-        window.display();
-    }
+    World().execute();
     return 0;
 }
