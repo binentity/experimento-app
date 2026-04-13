3bb97751d87df48bf4cc966aa6bdb319c10c4938
binentity <binentity@yandex.ru>
2026-04-13 12:41:56 +0300

    Move basic sfml window creation in execute method from world.h.

---
+++ world.h
@@ -0,0 +1,27 @@
+#ifndef EXPERIMENTO_APP_WORLD_H
+#define EXPERIMENTO_APP_WORLD_H
+
+#include "system.h"
+
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include <vector>
+
+constexpr unsigned int WIDTH = 2550;
+constexpr unsigned int HEIGHT = 1440;
+constexpr sf::Vector2u screenVector(WIDTH, HEIGHT);
+
+class World
+{
+    std::vector<std::unique_ptr<System>> systems;
+
+public:
+    void execute() const;
+    void update(float dt) const;
+
+    World() = default;
+    ~World() = default;
+};
+
+#endif //EXPERIMENTO_APP_WORLD_H
